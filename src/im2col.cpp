5c13a9edbeb9b306875ed95ce1838a450fac08e1
unknown <873175036@qq.com>
2022-11-03 21:27:52 +0800

    clear project

--- src/im2col.cpp
+++ src/im2col.cpp
@@ -12,17 +12,12 @@ void generate_map(int feature_h, int feature_w, int feature_c, int kernel_size, 
 	ap_uint<16> s = stride;
 	ap_uint<16> K = kernel_size;
 
-//	Pos** out = new Pos*[out_h];
-//	for(int i = 0; i < out_h; i++){
-//		out[i] = new Pos[out_w];
-//	}
 	for(int i = 0; i < out_h; i++){
 		for(int j = 0; j < out_w; j++){
 			ap_uint<16> num_C = i / (((Wi - K) / s + 1) * ((Hi - K) / s + 1)); // 第几个特征图
 			ap_uint<16> num_k = i % (((Wi - K) / s + 1) * ((Hi - K) / s + 1)); // 第几个卷积核
 			ap_uint<16> start = num_k / ((Wi - K) / s + 1) * Wi + num_k % ((Wi - K) / s + 1) ;
 			ap_uint<16> offset = j / K;
-			//out[i][j] = Pos(num_C, start + offset * Wi + j % K);
 			ap_uint<16> y = start + offset * Wi + j % K;
 			buf_map[i * out_w + j] = (num_C, y);
 		}
@@ -39,8 +34,6 @@ void im2col(int feature_h, int feature_w, int feature_c, int kernel_size, int st
 		int out_w, MTYPE buf_map[MAP_SIZE], DTYPE buf_result[BUF_SIZE], int read_addr, int write_addr){
 	for(int i = 0; i < out_h; i++){
 		for(int j = 0; j < out_w; j++){
-//			int ii = map[i][j].x;
-//			int jj = map[i][j].y;
 			ap_uint<16> ii = buf_map[i * out_w + j](31, 16);
 			ap_uint<16> jj = buf_map[i * out_w + j](15, 0);
 			buf_result[write_addr + i * out_w + j] = buf_result[read_addr + ii * feature_w * feature_h + jj];
